31ea3ef605b9d43a8023f2393131fc409e6f185c
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-03-06 09:25:30 +0400

    automatically use a type from `jest-config`

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-/**
- * @type {import('ts-jest').JestConfigWithTsJest['coverageReporters']}
- *
- * @see https://github.com/facebook/jest/blob/654949b5897c93cc3d7cf5d9c0efbb45c21a8c45/packages/jest-config/src/Defaults.ts#L28
- */
-const defaultCoverageReporters = require('jest-config').defaults.coverageReporters;
+/** @see https://github.com/facebook/jest/blob/654949b5897c93cc3d7cf5d9c0efbb45c21a8c45/packages/jest-config/src/Defaults.ts#L28 */
+const defaultCoverageReporters = (require('jest-config')).defaults.coverageReporters;
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
